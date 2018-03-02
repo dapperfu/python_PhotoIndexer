c39772f5dbfc123c4cdcb1f63f76a180f0ea244f
Jed <jed@m6700.local>
2018-03-02 18:02:41 -0500

    Added submodules. Started dev work

--- sync_sd.sh
+++ sync_sd.sh
@@ -7,9 +7,18 @@ ROOT=/pictures
 # Temporary local directory.
 DIR=`date --iso-8601="seconds"`
 
+# Cameras (especially "Action Cams" have a bad habit of corrupting 
+# a partition)
 fsck /dev/mmcblk0p1
+
+# Create a mountpoint.
 mkdir -p /mnt/sdcard
+
+# Mount the SD card.
 mount /dev/mmcblk0p1 /mnt/sdcard
+# Create a dest dir
 mkdir -p $ROOT/$DIR
+# Sync
 rsync -aP /mnt/sdcard/ $ROOT/$DIR/
+# Unmo
 umount /mnt/sdcard
