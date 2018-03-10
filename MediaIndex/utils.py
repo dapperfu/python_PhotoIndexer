6cf837c978efcc6c36c3f020142f553bf721cf12
Jed <jed@m6700.local>
2018-03-10 11:49:30 -0500

    .

--- MediaIndex/utils.py
+++ MediaIndex/utils.py
@@ -1,5 +1,12 @@
 import xxhash
-def get_xxhash(string=""):
+def get_str_xxhash(string=""):
     x = xxhash.xxh64()
     x.update(string)
     return x.hexdigest()
+
+def get_xxhash(fname):
+    hash64 = xxhash.xxh64()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash64.update(chunk)
+    return hash64.hexdigest()
