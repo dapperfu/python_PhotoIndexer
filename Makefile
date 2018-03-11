b05273b8b93e7a5349969034b5cc10ff11da5a8f
Jed <jed@8600k.local>
2018-03-11 19:18:38 -0400

    .

--- Makefile
+++ Makefile
@@ -1,7 +1,14 @@
-VENV = $(realpath $(dir $(firstword $(MAKEFILE_LIST))))
+CWD = $(realpath $(dir $(firstword $(MAKEFILE_LIST))))
+
+PROJ ?= $(notdir ${CWD})
+VENV ?= ${CWD}
+HOST ?= $(shell hostname).local
 PIP?=${VENV}/bin/pip
 PYTHON?=${VENV}/bin/python
 
+# Base python modules to install before everything else
+# Some projects need wheel, numpy and cython
+# before they will install correctly.
 BASE?=setuptools wheel numpy cython
 
 .DEFAULT: venv
@@ -10,7 +17,7 @@ ${PYTHON}: requirements.txt
 	python3 -mvenv ${VENV}
 	${PIP} install --upgrade pip
 	${PIP} install --upgrade ${BASE}
-	${PIP} install -r requirements.txt
+	${PIP} install --upgrade --requirement requirements.txt
 
 .PHONY: clean
 clean:
@@ -18,3 +25,12 @@ clean:
 
 requirements.txt:
 	@echo requirements.txt is missing.
+
+.PHONY:nb
+nb: ${PYTHON}
+	screen -S ${PROJ} -d -m bin/jupyter-notebook --ip=${HOST}
+
+.PHONY: debug
+debug:
+	$(info $${HOST}=${HOST})
+	$(info $${PROJ}=${PROJ})
--- MediaIndex/utils.py
+++ MediaIndex/utils.py
@@ -1,4 +1,32 @@
 import xxhash
+import exiftool
+import configparser
+import redis
+import json
+import get_files
+import rq
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+exif_cache = redis.StrictRedis(
+    host=config["redis"]["host"],
+    port=config["redis"]["port"],
+    db=config["redis"]["exif"],
+)
+xxhash_cache = redis.StrictRedis(
+    host=config["redis"]["host"],
+    port=config["redis"]["port"],
+    db=config["redis"]["xxhash"],
+)
+
+r = redis.StrictRedis(
+    host=config["redis"]["host"],
+    port=config["redis"]["port"],
+    db=config["redis"]["rq"],
+)
+q = rq.Queue(connection=r)
+
 def get_str_xxhash(string=""):
     x = xxhash.xxh64()
     x.update(string)
@@ -6,7 +34,44 @@ def get_str_xxhash(string=""):
 
 def get_xxhash(fname):
     hash64 = xxhash.xxh64()
-    with open(fname, "rb") as f:
+    with open(str(fname), "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash64.update(chunk)
     return hash64.hexdigest()
+
+def get_exif(fname):
+    with exiftool.ExifTool() as et:
+        return et.get_metadata(str(fname))
+
+def cache_xxhash(media_file):
+    XXHASH_ = xxhash_cache.get(str(media_file))
+    if XXHASH_ is None:
+        XXHASH = get_xxhash(str(media_file))
+        xxhash_cache.set(str(media_file), XXHASH)
+        print("Caching hash: {}".format(media_file))
+    else:
+        XXHASH = XXHASH_.decode()
+        print("Cached hash : {}".format(media_file))
+    return XXHASH
+
+def cache_exif(media_file):
+    EXIF_ = exif_cache.get(xxhash)
+    if  EXIF_ is None:
+        EXIF = get_exif(media_file)
+        exif_cache.set(xxhash, json.dumps(EXIF))
+        print("Caching EXIF: {}".format(media_file))
+    else:
+        EXIF = json.loads(EXIF_.decode())
+        print("Cached EXIF : {}".format(media_file))
+    return(EXIF)
+
+def scan_dir(root_dir):
+    media_dirs = get_files.get_dirs(root_dir, depth=1)
+    for media_dir in media_dirs:
+        if ".zfs" in media_dir:
+            continue
+        q.enqueue(scan_dir, media_dir)
+    media_files = get_files.get_files(root_dir, depth=1)
+    for media_file in media_files:
+        q.enqueue(cache_xxhash, media_file)
+        q.enqueue(cache_exif, media_file)
