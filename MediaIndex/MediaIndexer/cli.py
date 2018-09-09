5dc9959e6e5f56ccfdc70d95dbb4ae48939076b9
Jed <jed@8600k.local>
2018-09-08 23:08:35 -0400

    jed@8600k: Sun Sep  9 03:08:35 UTC 2018
    Former-commit-id: f8a20f683d86df5f4911e3d0944f92f1629ae331

--- MediaIndex/MediaIndexer/cli.py
+++ MediaIndex/MediaIndexer/cli.py
@@ -8,9 +8,16 @@ import click
 import click_config_file
 from .config import config
 
-
 @click.group()
 @click.version_option()
+def cli():
+    """```pydarknet2``` command line interface entry point.
+
+    darknet.py is a utility for interacting with pydarknet from the
+    command line.
+    """
+
+@click.command()
 @click.option('--name', default='World', help='Who to greet.')
 @click_config_file.configuration_option()
 def hello(name):
