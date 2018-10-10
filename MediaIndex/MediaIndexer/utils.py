34503dbc19df8eb07f4345e9734bbbef62cc2e73
Jed <jed@m6700.local>
2018-10-10 06:19:31 -0400

    jed@m6700: Wed Oct 10 09:31:27 UTC 2018
    Former-commit-id: 05db1404b0a43e0a72e1c51f2f80fdcc0ce1fe3d [formerly 707c7eb4e7708816e209be492fc54387f33e3b17]
    Former-commit-id: 7fbb0941ac4d6cf40dc669aaf9383226aaafeb95

--- MediaIndex/MediaIndexer/utils.py
+++ MediaIndex/MediaIndexer/utils.py
@@ -5,40 +5,8 @@ Generic utilities for use in MediaIndexer.
 """
 
 import io
-
-import exiftool
+import configparser
 from PIL import Image
-import xxhash
-import pydarknet2
-
-def get_xxhash(file_path):
-    """ Get the xxhash of a given file."""
-    hash64 = xxhash.xxh64()
-    with open(str(file_path), "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash64.update(chunk)
-    return hash64.hexdigest()
-
-
-def get_exif(file_path):
-    """Get the exif of a given file."""
-    with exiftool.ExifTool() as et:
-        return et.get_metadata(str(file_path))
-
-
-def get_thumbnail(file_path, size=(255, 255), pil_image=True):
-    if isinstance(file_path, bytes):
-        file_path = file_path.decode("UTF-8")
-
-    img = Image.open(file_path)
-    img.thumbnail(size)
-    if pil_image:
-        return img
-
-    with io.BytesIO() as buffer:
-        img.save(buffer, format="jpeg")
-        thumbnail = buffer.getvalue()
-    return thumbnail
 
 def pil_thumbnail(thumbnail_str):
     assert isinstance(thumbnail_str, bytes)
