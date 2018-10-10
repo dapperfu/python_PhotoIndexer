04f8df0e9534791fa45bf2063be35e17a5b02bbb
Jed <jed@m6700.local>
2018-10-10 09:13:06 -0400

    jed@m6700: Wed Oct 10 13:12:09 UTC 2018
    Former-commit-id: f353787f6e87d4fc3814df0f4b602ad0ac6fe762 [formerly cc7a6dbc96683a3a27d29aa55f01ca702b4ef820]
    Former-commit-id: 570d34de829c9616eaca26b5ab1236bd00aecfc1

--- MediaIndex/api.py
+++ MediaIndex/api.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Resource, Api
 from flask_bootstrap import Bootstrap
 from flask_restful import reqparse
+from flask import request
 
 app = Flask(__name__)
 api = Api(app)
