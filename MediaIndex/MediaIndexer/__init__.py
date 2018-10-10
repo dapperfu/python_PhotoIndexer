1f4bb205b9921c4c0bc2ded4fcef14763ac3ea4c
Jed <jed@m6700.local>
2018-10-10 01:31:58 -0400

    jed@m6700: Wed Oct 10 05:31:06 UTC 2018
    Former-commit-id: 2481f0dd0a0ff206c4a1465c7903d7d89b5db3d6 [formerly 4e9c49fd463b67c0e0f9f12fb2d91b587270ee07]
    Former-commit-id: 4438bf93dabc84be0fc8670faf8d0d74c815818d

--- MediaIndex/MediaIndexer/__init__.py
+++ MediaIndex/MediaIndexer/__init__.py
@@ -2,3 +2,12 @@
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+import os
+
+class MediaIndexer(object):
+    def __init__(self, config):
+        self.config=config
+        
+        
+    def __repr__()
