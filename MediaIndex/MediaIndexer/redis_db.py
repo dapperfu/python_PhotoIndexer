d42eff0cb13c19bd2130ca357b988aeeae103fb9
Jed <jed@m6700.local>
2018-10-10 01:37:07 -0400

    jed@m6700: Wed Oct 10 05:31:06 UTC 2018
    Former-commit-id: 69257d49ae0d2bdc5c3e4a3515773233ea0dd4de [formerly de1dfb717e8c0cbbe9a69ad860536f1042678f5d]
    Former-commit-id: 7cfb8b67e8ddba6f135af5ebc5e88c7a4b5a39a0

--- MediaIndex/MediaIndexer/redis_db.py
+++ MediaIndex/MediaIndexer/redis_db.py
@@ -18,34 +18,35 @@ import sys
 from docopt import docopt
 import redis
 
-cfg = os.path.join("config.ini")
+def load_databases(cfg="config.ini"):
 
-config = configparser.ConfigParser()
-config.read(cfg)
-
-databases = dict()
-for key in config["redis"].keys():
-    if key in ["host", "port"]:
-        continue
-    db_ = redis.StrictRedis(
-        host=config["redis"]["host"],
-        port=config["redis"]["port"],
-        db=config["redis"][key],
-    )
-    databases[key] = db_
-
-this = sys.modules[__name__]
-for db_name, db in databases.items():
-    print("Loaded: {}".format(db_name))
-    setattr(this, db_name, db)
-
-
-def flush_keys():
+    config = configparser.ConfigParser()
+    config.read(cfg)
+    
+    databases = dict()
+    for key in config["redis"].keys():
+        if key in ["host", "port"]:
+            continue
+        db_ = redis.StrictRedis(
+            host=config["redis"]["host"],
+            port=config["redis"]["port"],
+            db=config["redis"][key],
+        )
+        databases[key] = db_
+    return databases
+    
+    this = sys.modules[__name__]
+    for db_name, db in databases.items():
+        print("Loaded: {}".format(db_name))
+        setattr(this, db_name, db)
+    
+    
+def flush_keys(databases):
     for db_name, db in databases.items():
         print("{} db: flushing".format(db_name))
 
 
-def key_count():
+def key_count(databases):
     for db_name, db in databases.items():
         print("{} db: {} keys".format(db_name, db.dbsize()))
 
