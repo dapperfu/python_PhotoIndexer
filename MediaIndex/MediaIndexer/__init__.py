a88c29e81184848f91eda830e73f71cfd4d49f98
Jed <jed@m6700.local>
2018-10-10 06:16:33 -0400

    jed@m6700: Wed Oct 10 09:31:27 UTC 2018
    Former-commit-id: 660af7992484ed57e27198ba83905c90a4297c88 [formerly 8de851b55f28a37591a6d008dabfed18ebe9d4cf]
    Former-commit-id: a4a4759ffbcedd23bc35f7015e6154c89bb963bd

--- MediaIndex/MediaIndexer/__init__.py
+++ MediaIndex/MediaIndexer/__init__.py
@@ -36,6 +36,7 @@ class MediaIndexer(RedisCacheMixin, CacherMixin):
 
     @cached_property.cached_property
     def classifier(self):
+        print("Network
         return pydarknet2.Classifier("cfg/coco.data", "cfg/yolov3.cfg", "/opt/weights/yolov3.weights", root="/tmp/darknet")
 
     def objects(self, file_path):
