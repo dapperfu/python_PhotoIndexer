3e96b96e113a9f82c1514acb9f6d358023700213
Jed <jed@m6700.local>
2018-10-10 06:32:24 -0400

    jed@m6700: Wed Oct 10 09:31:27 UTC 2018
    Former-commit-id: 0eedf531844a11ac6d32cfdf24d6e4c0be72af3b [formerly 6c0e60da2249f899733c5a283c27de16529f1b86]
    Former-commit-id: 1b5ca80ab1ee079b9e790fcabd86555c8ba4eeae

--- MediaIndex/MediaIndexer/redis_db.py
+++ MediaIndex/MediaIndexer/redis_db.py
@@ -6,14 +6,14 @@ import os
 import sys
 
 import redis
-from .config import read_config
+from .utils import read_config
 
 def load_databases(config):
     """."""
 
     if isinstance(config, configparser.ConfigParser):
         config = config
-    if config is None:
+    elif isinstance(config, str):
         config = read_config(config_file=config_file)
     databases = dict()
     for key in config["redis"].keys():
