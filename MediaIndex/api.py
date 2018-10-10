e87d6d6ae19cb861527e236a90a498a3bdd88cc2
Jed <jed@m6700.local>
2018-10-10 09:14:02 -0400

    jed@m6700: Wed Oct 10 13:12:09 UTC 2018
    Former-commit-id: b28dc206118b5f10a1f70a9b00f7e171dd21a2a7 [formerly ba60867274d33f2ed54cbf6a1b0fc2af20abf979]
    Former-commit-id: fcb8f550bb55d688cd752d36be07dde80586b604

--- MediaIndex/api.py
+++ MediaIndex/api.py
@@ -52,7 +52,7 @@ api.add_resource(exif, '/api/exif')
 @app.route('/thumbnails/')
 def null():
     path = request.args.get('path')
-    return request.args
+    return path
 
 
 # <image src="data:image/png;base64,' + caffe.draw.draw_net(net, "UD").encode("base64") + '" style="max-width:100%" />
