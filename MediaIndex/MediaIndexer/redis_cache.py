b77b93c21888b455c96afc08623ce55d9bd65625
Jed <jed@m6700.local>
2018-10-10 01:53:05 -0400

    jed@m6700: Wed Oct 10 05:31:06 UTC 2018
    Former-commit-id: 4fefba0ff53d00b553fd486696c3647ba2bc52c7 [formerly a238aa93ec2891260aed3555800524513cb3bc22]
    Former-commit-id: 2213f48b117d7e7bec83d8a08ae96dc371585e47

--- MediaIndex/MediaIndexer/redis_cache.py
+++ MediaIndex/MediaIndexer/redis_cache.py
@@ -90,29 +90,29 @@ class RedisCacheMixin(object):
 import get_files
 import os
 import rq
-
-def cache_dir(root_dir):
-    db_ = "rq"
-    connection = redis_db.databases[db_]
-    queue = rq.Queue(connection=connection)
-    # Scan for directories in the given root directory, to a depth of 1
-    media_dirs = get_files.get_dirs(root_dir, depth=1)
-    # For each found directory:
-    for media_dir in media_dirs:
-        # Don't scan zfs snap directories
-        if ".zfs" in media_dir:
-            continue
-        # Don't follow symbolic links.
-        if os.path.islink(media_dir):
-            continue
-        # Queue scanning the folder.
-        queue.enqueue(cache_dir, media_dir)
-    # Scan given directory for files, to a depth of 1
-    media_files = get_files.get_files(root_dir, depth=1)
-    # For each media_file:
-    for media_file in media_files:
-        # Queue scanning the media's exif data.
-        queue.enqueue(cache_thumbnail, media_file)
+class CacherMixin(object):
+    def cache_dir(root_dir):
+        db_ = "rq"
+        connection = redis_db.databases[db_]
+        queue = rq.Queue(connection=connection)
+        # Scan for directories in the given root directory, to a depth of 1
+        media_dirs = get_files.get_dirs(root_dir, depth=1)
+        # For each found directory:
+        for media_dir in media_dirs:
+            # Don't scan zfs snap directories
+            if ".zfs" in media_dir:
+                continue
+            # Don't follow symbolic links.
+            if os.path.islink(media_dir):
+                continue
+            # Queue scanning the folder.
+            queue.enqueue(cache_dir, media_dir)
+        # Scan given directory for files, to a depth of 1
+        media_files = get_files.get_files(root_dir, depth=1)
+        # For each media_file:
+        for media_file in media_files:
+            # Queue scanning the media's exif data.
+            queue.enqueue(cache_thumbnail, media_file)
 
 
 if __name__ == "__main__":
