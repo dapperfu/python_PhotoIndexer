8ffcfeb6972156c516476618e417be72651707e8
Jed <jed@m6700.local>
2018-10-10 05:35:39 -0400

    jed@m6700: Wed Oct 10 09:31:27 UTC 2018
    Former-commit-id: 9c46f1106312997bbe118f54685b16aa4368ed4a [formerly 27808cc8ff2ecfeefe6359ee709731d76beed934]
    Former-commit-id: 3ba3ad1fea53dfa35fa426fe4fd4d5c4528671d5

--- MediaIndex/MediaIndexer/worker.py
+++ MediaIndex/MediaIndexer/worker.py
@@ -5,24 +5,24 @@
 import MediaIndexer
 from MediaIndexer import redis_cache
 
-def cache_xxhash(config, file_path):
-    databases = MediaIndexer.redis_db.load_databases(config)
+def cache_xxhash(config_file, file_path):
+    databases = MediaIndexer.redis_db.load_databases(config_file)
     cfg = {
         "file_path": file_path,
         "databases": databases,
     }
     redis_cache._get_xxhash(**cfg)
 
-def cache_exif(config, file_path):
-    databases = MediaIndexer.redis_db.load_databases(config)
+def cache_exif(config_file, file_path):
+    databases = MediaIndexer.redis_db.load_databases(config_file)
     cfg = {
         "file_path": file_path,
         "databases": databases,
     }
     redis_cache._get_exif(**cfg)
 
-def cache_thumbnail(config, file_path):
-    databases = MediaIndexer.redis_db.load_databases(config)
+def cache_thumbnail(config_file, file_path):
+    databases = MediaIndexer.redis_db.load_databases(config_file)
     cfg = {
         "file_path": file_path,
         "databases": databases,
