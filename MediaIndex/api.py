4fd7c2e16eb44bac83069a24f01662706c4c3251
Jed <jed@m6700.local>
2018-10-10 10:20:32 -0400

    isorted and blackened python files
    Former-commit-id: a166b49b848567906f0f5776b1fefad4ece274ce [formerly 31ce2b148c8aaa32fde037d92afd81659331245e]
    Former-commit-id: 671022c0de8a99235ecc5686a77500bc67620a63

--- MediaIndex/api.py
+++ MediaIndex/api.py
@@ -1,21 +1,21 @@
-from flask import Flask
-from flask_restful import Resource, Api
-from flask_bootstrap import Bootstrap
-from flask_restful import reqparse
-from flask import request
-from flask import make_response
-from flask import Flask, send_file
+import io
+import json
+import os
 
+from flask import Flask, make_response, request, send_file
+from flask_bootstrap import Bootstrap
+from flask_restful import Api, reqparse, Resource
+
+import MediaIndexer
+import MediaIndexer.redis_cache
 
 app = Flask(__name__)
 api = Api(app)
 
 parser = reqparse.RequestParser()
-parser.add_argument('path')
+parser.add_argument("path")
 
-import MediaIndexer
 
-import os
 cfg = os.path.abspath("config_m6700.ini")
 assert os.path.exists(cfg)
 
@@ -29,19 +29,21 @@ def debug(self, *args, **kwargs):
         print("{}: {}".format(i, arg))
 
     for key, value in kwargs.items():
-        print("{}: {}".format(key,value))
+        print("{}: {}".format(key, value))
     return args
+
 
 class xxhash(Resource):
     def get(self):
         args = parser.parse_args()
-        args["xxhash"]=indexer.get_xxhash(args["path"])
+        args["xxhash"] = indexer.get_xxhash(args["path"])
         return args
+
 
 class exif(Resource):
     def get(self):
         args = parser.parse_args()
-        args["exif"]=indexer.get_exif(args["path"])
+        args["exif"] = indexer.get_exif(args["path"])
         return args
 
 
@@ -49,35 +51,35 @@ for method in ["delete", "put", "post"]:
     setattr(xxhash, method, debug)
     setattr(exif, method, debug)
 
-api.add_resource(xxhash, '/api/xxhash')
-api.add_resource(exif, '/api/exif')
-import io
-@app.route('/thumbnails')
+api.add_resource(xxhash, "/api/xxhash")
+api.add_resource(exif, "/api/exif")
+
+
+@app.route("/thumbnails")
 def thumbnails():
-    path = request.args.get('path')
+    path = request.args.get("path")
 
     thumbnail = indexer.get_thumbnail(path, pil_image=False)
 
-    return send_file(
-                     io.BytesIO(thumbnail),
-                     mimetype='image/jpg'
-               )
+    return send_file(io.BytesIO(thumbnail), mimetype="image/jpg")
 
-import MediaIndexer.redis_cache
-@app.route('/thumbnails/<string:xxhash>.jpg')
+
+@app.route("/thumbnails/<string:xxhash>.jpg")
 def thumbnails2(xxhash):
-    thumbnail = MediaIndexer.redis_cache._get_thumbnail(file_path="", file_hash=xxhash, databases=indexer.databases)
-    return send_file(
-                     io.BytesIO(thumbnail),
-                     mimetype='image/jpg'
-            )
+    thumbnail = MediaIndexer.redis_cache._get_thumbnail(
+        file_path="", file_hash=xxhash, databases=indexer.databases
+    )
+    return send_file(io.BytesIO(thumbnail), mimetype="image/jpg")
 
-import json
-@app.route('/exif/<string:xxhash>.json')
+
+@app.route("/exif/<string:xxhash>.json")
 def exif2(xxhash):
-    exif=MediaIndexer.redis_cache._get_exif(file_path="", file_hash=xxhash, databases=indexer.databases)
+    exif = MediaIndexer.redis_cache._get_exif(
+        file_path="", file_hash=xxhash, databases=indexer.databases
+    )
     return json.dumps(exif)
 
+
 # <image src="data:image/png;base64,' + caffe.draw.draw_net(net, "UD").encode("base64") + '" style="max-width:100%" />
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
