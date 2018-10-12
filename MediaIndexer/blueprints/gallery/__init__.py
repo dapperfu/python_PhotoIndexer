ee00d560a12a7ca102d684ab7638e34b81509296
jed <jed+MediaIndex@m6700-LINUX>
2018-10-12 15:58:44 -0400

    m6700: Fri Oct 12 19:38:23 UTC 2018
    Former-commit-id: 9aaceca05b2f579db3d508a313663726655416f3 [formerly 2449c101d81554555eb596a1a8b2c02464822f3a]
    Former-commit-id: 6dbbf754d58a62db0af5eab3d597ba0e096521a9

--- MediaIndexer/blueprints/gallery/__init__.py
+++ MediaIndexer/blueprints/gallery/__init__.py
@@ -31,6 +31,20 @@ def index(**kwargs):
         print(validate(f.name))
     return html
 
+@gallery.route('/<page>/items.json')
+def items(**kwargs):
+    r = os.path.abspath(os.path.join(os.curdir, kwargs["page"]))
+    directories = get_files.get_dirs(directory = r, depth=1, absolute=True)
+    images = get_files.get_files(directory = r, extensions=['.jpg'], depth=1, absolute=True)
+    for key, item in kwargs.items():
+        print("{}: {}".format(key, item))
+    html = render_template('index.html', directorys=directories, images=images)
+    with tempfile.NamedTemporaryFile(mode="w", delete=True) as f:
+        f.write(html)
+        f.flush()
+        print(validate(f.name))
+    return html
+
 @gallery.route('/style.css')
 def css(**kwargs):
     for key, item in kwargs.items():
