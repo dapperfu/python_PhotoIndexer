d9342ec6f78a996c0892702e679521f5fff769a2
jed <jed+MediaIndex@m6700-LINUX>
2018-10-12 18:00:35 -0400

    m6700: Fri Oct 12 19:38:23 UTC 2018
    Former-commit-id: 7cae54930fb7f45c857b532ef34f9317e24a2a17 [formerly a02ec601f3b4c1d1de90e15e2173795550ce64dc]
    Former-commit-id: dab8c44118fd99a3fa5c39bcc7988524d4c34e93

--- MediaIndexer/blueprints/thumbnails/__init__.py
+++ MediaIndexer/blueprints/thumbnails/__init__.py
@@ -3,7 +3,7 @@ from jinja2 import TemplateNotFound
 import json
 
 thumbnails = Blueprint('thumbnails', __name__, template_folder='templates', url_prefix='/thumbnails')
-
+import os
 @thumbnails.route('/<int:size>/<string:xxhash>.jpg')
 def get_thumbnail(xxhash, size=128):
     config_file = os.environ["MEDIAINDEXER_CFG"]
