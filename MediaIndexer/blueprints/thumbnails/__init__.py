537a03cf2dda3390813d9bf6cf5eed74f1893d89
jed <jed+MediaIndex@m6700-LINUX>
2018-10-12 17:57:28 -0400

    m6700: Fri Oct 12 19:38:23 UTC 2018
    Former-commit-id: 07e701e1d7514b72be2eef8f51a7697c5c659eda [formerly 2d17ed8125661d5632ed38a50189243768ca063a]
    Former-commit-id: bb27e447938132c5be486c717eee363a936cf792

--- MediaIndexer/blueprints/thumbnails/__init__.py
+++ MediaIndexer/blueprints/thumbnails/__init__.py
@@ -5,5 +5,11 @@ import json
 thumbnails = Blueprint('thumbnails', __name__, template_folder='templates', url_prefix='/thumbnails')
 
 @thumbnails.route('/')
-def index():
-    return "Hello Thumbnails."
+def get_thumbnail(xxhash):
+    config_file = os.environ["MEDIAINDEXER_CFG"]
+    databases = MediaIndexer.redis_utils.load_databases(config_file)
+
+    thumbnail = MediaIndexer.redis_cache._get_thumbnail(
+        file_path="", file_hash=xxhash, databases=databases
+    )
+    return send_file(io.BytesIO(thumbnail), mimetype="image/jpg")
