8e57607a4609e225fd3c090992cacd56e1832d31
jed <jed+MediaIndex@m6700-LINUX>
2018-10-12 03:18:10 -0400

    m6700: Fri Oct 12 06:54:08 UTC 2018
    Former-commit-id: 4489f74de13202a9fbea035d111cb6cc0cf80bb7 [formerly c14267c003bdcfcb2261fe43306493a2a4e970f3]
    Former-commit-id: 1ca682c76c2cabdfae09a43e0552e2f2bfb382e5

--- MediaIndex/MediaIndexer/blueprints/gallery/__init__.py
+++ MediaIndex/MediaIndexer/blueprints/gallery/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 import json
 
-gallery = Blueprint('gallery', __name__, template_folder='templates', url_prefix='/g')
+gallery = Blueprint('gallery', __name__, template_folder='templates', url_prefix='/admin2')
 
 @gallery.route('/')
 def index():
