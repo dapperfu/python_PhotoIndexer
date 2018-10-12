d237764edfae29cf6f43f98b68524171f79d0735
jed <jed+MediaIndex@m6700-LINUX>
2018-10-12 03:08:20 -0400

    m6700: Fri Oct 12 06:54:08 UTC 2018
    Former-commit-id: 3f678f7571abf4bd2453274907fd06c91911581e [formerly 027084f95a24c95060b5ef1a0b9271001123f4e5]
    Former-commit-id: 8b4fe51dae75ecf7d820e0ff82002f6878b4d164

--- MediaIndex/MediaIndexer/blueprints/admin/__init__.py
+++ MediaIndex/MediaIndexer/blueprints/admin/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 import json
 
-admin = Blueprint('admin', __name__, url_prefix='/admin')
+admin = Blueprint('admin', __name__, template_folder='templates', url_prefix='/admin')
 @admin.route('/')
 def index():
     return "Hello Admin"
