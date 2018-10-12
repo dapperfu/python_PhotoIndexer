2f2f6f1ffc22b083cd9a57a53c7c9007daaadc1e
jed <jed+MediaIndex@m6700-LINUX>
2018-10-12 03:22:28 -0400

    m6700: Fri Oct 12 06:54:08 UTC 2018
    Former-commit-id: a422bb7705eda6a032186b3c24c29cbc27a2ff86 [formerly e68cedfb08f773d8aa1dce62e5a01c1ae09fef5c]
    Former-commit-id: a19cda0d862993338d702c6f5211df0cea262894

--- MediaIndex/MediaIndexer/flask.py
+++ MediaIndex/MediaIndexer/flask.py
@@ -12,8 +12,7 @@ import werkzeug.exceptions
 
 import MediaIndexer
 import MediaIndexer.redis_utils
-from MediaIndexer.blueprints.gallery import gallery
-from MediaIndexer.blueprints.admin import admin
+from MediaIndexer.blueprints import gallery, admin
 
 parser = reqparse.RequestParser()
 parser.add_argument("path")
