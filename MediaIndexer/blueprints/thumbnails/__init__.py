8dd19ed7e648c6ec85eb777b508462aa72954028
jed <jed+MediaIndex@m6700-LINUX>
2018-10-12 17:29:45 -0400

    m6700: Fri Oct 12 19:38:23 UTC 2018
    Former-commit-id: 8329dbc891f1289a7079e29df9b49f5c3cdf0a86 [formerly 17a5dcd668d504135d401ea83a79bfb435343229]
    Former-commit-id: f3d96228afd28abec7bfc4a563ffa06257082007

--- MediaIndexer/blueprints/thumbnails/__init__.py
+++ MediaIndexer/blueprints/thumbnails/__init__.py
@@ -2,8 +2,8 @@ from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 import json
 
-thumbnail_blueprint = Blueprint('thumbnails', __name__, template_folder='templates', url_prefix='/admin')
+thumbnails = Blueprint('thumbnails', __name__, template_folder='templates', url_prefix='/admin')
 
-@thumbnail_blueprint.route('/')
+@thumbnails.route('/')
 def index():
     return "Hello Thumbnails."
