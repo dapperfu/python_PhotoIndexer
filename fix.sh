2a2b89044f26864675459471b3175c600ec077b5
Jed <jed@4770k>
2019-05-05 02:54:03 -0400

    Computer. Hello Computer.
    Former-commit-id: 447969b82af57fe5292dda3a818a6ab6d3215659 [formerly 9e74dfec06504cad9e74b7685e6a60f16c42537e]
    Former-commit-id: 9d2f340183cedf0d7032f6db51349578bc0ba001

--- fix.sh
+++ fix.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 isort --apply --settings-path ../setup.cfg *.py
-black --py36 --line-length 79 *.py
-
+black --target-version py37 --line-length 79 *.py
 git add *.py
 git commit -m "isorted and blackened python files"
